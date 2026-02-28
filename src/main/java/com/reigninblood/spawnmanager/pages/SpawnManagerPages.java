65f43d308fde32aee6f9a8044f0c5ba593fdde5e
Reign-in-blood <canni.gaming@gmail.com>
2026-02-28 16:45:16 +0100

    Use Pages-relative custom UI document paths

--- src/main/java/com/reigninblood/spawnmanager/pages/SpawnManagerPages.java
+++ src/main/java/com/reigninblood/spawnmanager/pages/SpawnManagerPages.java
@@ -36,8 +36,8 @@ public final class SpawnManagerPages extends BasicCustomUIPage {
     private static final Type MAP_TYPE = new TypeToken<Map<String, String>>() {}.getType();
 
     // UI paths
-    private static final String PAGE_PATH = "UI/Custom/Pages/SpawnManagerPage.ui";
-    private static final String ROW_PATH = "UI/Custom/Pages/MobRow.ui";
+    private static final String PAGE_PATH = "Pages/SpawnManagerPage.ui";
+    private static final String ROW_PATH = "Pages/MobRow.ui";
 
     // Fichier cible (test)
     private static final String FILE_NAME = "Spawns_Zone1_Forests_Predator.json";
